2f5fb051f0a4ae247c72ad38bd7a7e1acf13a2c1
L00113835 <33056781+L00113835@users.noreply.github.com>
2017-10-25 15:46:58 +0100

    Update App.java

--- mvnSimpleProj/src/main/java/mvnSimpleProj/App.java
+++ mvnSimpleProj/src/main/java/mvnSimpleProj/App.java
@@ -15,6 +15,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.printlh("Hello I have github working");
 	}
 
 }
